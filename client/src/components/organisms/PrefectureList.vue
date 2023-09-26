c0e74c496667dfd36e9dbb97a38a6bd6405f310e
oba <oba-mitsuki@ksc.co.jp>
2023-09-26 13:39:36 +0900

    day3

--- client/src/components/organisms/PrefectureList.vue
+++ client/src/components/organisms/PrefectureList.vue
@@ -2,8 +2,23 @@
 import { onMounted, ref } from "vue"
 import axiosInstance from "@/utils/axiosSettings"
 import { PrefectureDisplay } from "@/types/prefecture"
+
+// const chiba = ref("千葉県")
+
 // TODO: prefectureで千葉県を表示してみましょう
-const prefecture = ref<PrefectureDisplay>({})
+const prefecture = ref<PrefectureDisplay>({
+  prefCode: 0,
+  prefName: "千葉県",
+  isCheck: false
+})
+
+// function change() {
+//   prefecture.value.isCheck = !prefecture.value.isCheck
+// }
+
+// function change(eve: boolean) {
+//   prefecture.value.isCheck = eve
+// }
 
 onMounted(() => {
   // TODO: 全県取得のAPIへリクエストを送ってみましょう!
@@ -13,8 +28,24 @@ onMounted(() => {
 <template>
   <div class="prefecture-container">
     <h3>都道府県</h3>
+    <!-- <div>{{ chiba }}</div> -->
     <div class="prefecture-flex">
       <!-- TODO: 県を表示してみましょう -->
+      {{ prefecture.prefName }}
+      {{ prefecture.isCheck }}
+      <!-- <input
+        type="checkbox"
+        @change="prefecture.isCheck = !prefecture.isCheck"
+      /> -->
+      <!-- <input
+        type="checkbox"
+        checked="prefecture.isCheck"
+        value="prefecture.isCheck"
+        @change="change(!prefecture.isCheck)"
+      /> -->
+      <input v-model="prefecture.isCheck" type="checkbox" />
+      <!-- <input type="checkbox" @input="change" /> -->
+      <div v-if="prefecture.isCheck">isChecked</div>
     </div>
   </div>
 </template>
